05c7bdc66a09fc35c7b35a2ee10aa552db467f23
ArcticFoox <jongsin16@naver.com>
2024-07-18 17:31:32 +0900

    feat: alarm 및 log 구현 중

--- src/main/java/org/example/bigdata/config/SlackWebhookConfig.java
+++ src/main/java/org/example/bigdata/config/SlackWebhookConfig.java
@@ -1,19 +1,15 @@
 package org.example.bigdata.config;
 
 import com.slack.api.Slack;
-import com.slack.api.methods.MethodsClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SlackWebhookConfig {
 
-    @Value("${slack.notification.url}")
-    private String url;
-
     @Bean
     public Slack slack(){
         return Slack.getInstance();
     }
+
 }
